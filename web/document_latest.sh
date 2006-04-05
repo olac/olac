eadd54f3622349fd8028170014a9893987ec8cae
stevenbird <stevenbird@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2006-04-05 09:44:34 +0000

    git-svn-id: https://olac.googlecode.com/svn/@43 e9cfdc32-c5ba-11dd-9551-afe151041c70

---
+++ web/document_latest.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+doc=$1
+date=$2
+
+### Usage
+
+if test -z "$doc" -o -z "$date" ; then
+  cat <<END
+usage: $0 <doc> <date>
+
+  <doc>  the basename of the document (e.g. "metadata")
+  <date> the date of the latest version (e.g. "20040201")
+
+  Run this from the document directory
+  (OLAC/, REC/, NOTE/)
+
+END
+  exit 1
+fi
+
+### Link latest doc
+
+ln -s -f $doc-$date.xml $doc.xml
+ln -s -f ../$doc-$date.html $doc.html
