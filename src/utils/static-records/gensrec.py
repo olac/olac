804a6ca6a9dbfc2edf4805d6f0900fbbb959f3bc
haepal <haepal@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2006-06-16 18:04:33 +0000

    static record pages generation tool and xsl stylesheet
    git-svn-id: https://olac.googlecode.com/svn/@175 e9cfdc32-c5ba-11dd-9551-afe151041c70

---
+++ src/utils/static-records/gensrec.py
@@ -0,0 +1,46 @@
+"""
+Generates static record pages.
+
+input:  ListRecords output
+output: Static record pages
+"""
+
+import sys
+from cStringIO import StringIO
+import os.path
+
+record = """<record
+     xmlns:olac="http://www.language-archives.org/OLAC/1.0/"
+     xmlns:dc="http://purl.org/dc/elements/1.1/"
+     xmlns:dcterms="http://purl.org/dc/terms/"
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+"""
+     
+flag = False
+f = file(sys.argv[1])
+l = f.readline()
+g = None
+while l:
+    if '<record>' in l:
+        g = StringIO()
+        g.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        g.write('<?xml-stylesheet type="text/xsl" href="/static-records/staticrecord.xsl" ?>\n')
+        l = record
+    elif '<identifier>' in l:
+        g.write(l)
+        l = f.readline()
+        oaiid = l.strip()
+    elif '<olac:olac ' in l:
+        l = '    <olac:olac>\n'
+    elif '</record>' in l:
+        g.write(l)
+        a = oaiid.split('/')
+        for d in a[:-1]:
+            if not os.path.exists(d):
+                os.mkdir(d)
+        file(oaiid+".xml","w").write(g.getvalue())
+        g = None
+        
+    if g: g.write(l)
+
+    l = f.readline()
