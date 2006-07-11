871dff0882764d1efec99338482792d40b7e09c2
badenh <badenh@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2006-07-11 01:59:20 +0000

    updating for analytics
    git-svn-id: https://olac.googlecode.com/svn/@183 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- web/tools/reports/archiveReport.php
+++ web/tools/reports/archiveReport.php
@@ -1088,5 +1088,12 @@ print "<img width=700 src=$graph>";
 
 ?>
 
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-427085-5";
+urchinTracker();
+</script>
+
 </body>
 </html>
