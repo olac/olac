fb6f2012b0440ee003235417e9cbd45cbbed6085
haepal <haepal@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2008-07-30 21:15:02 +0000

    current (right before 1.1 updates) version of OLACA
    git-svn-id: https://olac.googlecode.com/svn/@511 e9cfdc32-c5ba-11dd-9551-afe151041c70

---
+++ web/cgi-bin/olaca3.pl
@@ -0,0 +1,67 @@
+#! /usr/bin/perl -Iolac_suite/3
+#! /pkg/p/perl5.6.0/bin/perl -Iolac_suite/3:/mnt/unagi/speechd8/ldc/wwwhome/htdocs/language-archives/lib/site-perl
+
+use OLAC::Aggregator;
+
+my $dbinfofile = "/home/olac/.dbinfo_olac2";
+my $identify = "olac_suite/identify3.xml";
+
+sub read_input {
+    my ($buffer, @pairs, $pair, $name, $value, %FORM);
+    # Read in text
+    $ENV{'REQUEST_METHOD'} =~ tr/a-z/A-Z/;
+    if ($ENV{'REQUEST_METHOD'} eq "POST") {
+        read(STDIN, $buffer, $ENV{'CONTENT_LENGTH'});
+    }
+    else {
+        $buffer = $ENV{'QUERY_STRING'};
+    }
+    # Split information into name/value pairs
+    @pairs = split(/&/, $buffer);
+    foreach $pair (@pairs) {
+        ($name, $value) = split(/=/, $pair);
+        $value =~ tr/+/ /;
+        $value =~ s/%(..)/pack("C", hex($1))/eg;
+        $FORM{$name} = $value;
+    }
+    if ($ENV{SERVER_PORT} eq "80") {
+      $FORM{baseURL} = "http://$ENV{SERVER_NAME}$ENV{REQUEST_URI}";
+    }
+    else {
+      $FORM{baseURL} = "http://$ENV{SERVER_NAME}:$ENV{SERVER_PORT}$ENV{REQUEST_URI}";
+    }
+    $FORM{baseURL} =~ s/\?.*//;
+    \%FORM;
+}
+
+sub main {
+    my $dp = new OLAC::Aggregator($dbinfofile, $identify);
+    my $request = read_input();
+
+    my $doc = $dp->serve_request($request);
+
+    if ($request->{verb} eq "Document" || not exists $request->{verb}) {
+	print "Content-type: text/html\n\n";
+	open(HTM, "olaca.htm");
+	while (<HTM>) {
+	    print $_;
+	}
+    }
+    elsif ($doc) {
+        print "Content-type: text/xml\n\n";
+        print $doc->toString;
+    }
+    else {
+        print "Content-type: text/html\n\n";
+        my $error = new HTTP::Response;
+        $error->code(400);
+        $error->message
+            ("Bad request" .
+             "<p>http://$ENV{SERVER_NAME}$ENV{REQUEST_URI}" .
+             "<p>$request->{error}");
+        print $error->error_as_HTML;
+    }
+}
+
+main;
+
