89a122288533dd8d0d2145e31e16f83263340775
StevenBird1 <StevenBird1@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-06-14 19:57:06 +0000

    Edward's work on a metadata classifier
    git-svn-id: https://olac.googlecode.com/svn/@862 e9cfdc32-c5ba-11dd-9551-afe151041c70

---
+++ src/classifier/oai_classifier.py
@@ -0,0 +1,117 @@
+"""
+Build a classifier that determines the appropriate label for a
+record, based on its features.
+"""
+
+from tabdbreader import TabDBCorpusReader
+import nltk
+
+######################################################################
+## Customization Constants
+######################################################################
+
+TRAIN_PERCENT = 50
+"""Indicates what percent of the corpus should be used for training
+   (vs testing)."""
+
+ALGORITHM = 'MEGAM'
+"""Use megam for training the maxent classifier, since it's fast and
+   does a good job.  If you don't want to install megam, then change
+   this to None to use the default algorithm, or to any other supported
+   algorithm (eg 'TADM' or 'LBFGSB')."""
+
+######################################################################
+## Feature Detector
+######################################################################
+
+def feature_detector(record):
+    """
+    Given an OLAC record, return a feature dictionary indicating
+    what information should be used by the machine learning
+    algorithm to determine the label for a given record.
+    """
+    # Note: several global variables used by this feature detector
+    # (e.g., FIELDS and WORD_FEATURES) are defined below, based on
+    # a scan through all of the training data.
+    
+    features = {'alwayson': True}
+    for field in FIELDS:
+        features['has(%s)' % field] = (field in record)
+
+    if 'description' in record:
+        record_words = set(record['description'].split())
+        for word in WORD_FEATURES:
+            features['descr(%s)' % word] = (word in record_words)
+
+    return features
+
+######################################################################
+## Helper Functions
+######################################################################
+
+def find_word_features(records):
+    """
+    Return a list of words that appear in at least 3 separate
+    records' description fields.
+    """
+    wordfreq = nltk.FreqDist()
+    for record in records:
+        if 'description' in record:
+            for word in set(record['description'].split()):
+                wordfreq.inc(word)
+    return [w for w in wordfreq if wordfreq[w] > 3]
+
+def find_fields(records):
+    """
+    Return a list of all fields that are used by the given records.
+    """
+    fields = set()
+    for record in records:
+        fields.update(record)
+    return fields
+        
+
+
+######################################################################
+## Main
+######################################################################
+
+if __name__ == '__main__':
+    # Read the corpora.
+    reader = TabDBCorpusReader('oai_classifier_trn', '.*db\.tab')
+    
+    olac_records = reader.records('olacdb.tab')
+    olac_train = olac_records[:len(olac_records)*TRAIN_PERCENT/100]
+    olac_test = olac_records[len(olac_records)*TRAIN_PERCENT/100:]
+    
+    oai_records = reader.records('oaidb.tab')
+    oai_train = oai_records[:len(oai_records)*TRAIN_PERCENT/100]
+    oai_test = oai_records[len(oai_records)*TRAIN_PERCENT/100:]
+    
+    # Scan the records 
+    WORD_FEATURES = find_word_features(olac_train + oai_train)
+    FIELDS = find_fields(olac_train + oai_train)
+    
+    # Tag the records with a label (yes/no depending on whether it came
+    # from olac or oai), and apply the feature detector.
+    fd = feature_detector
+    train_data = (
+        nltk.util.LazyMap(lambda v: (fd(v), 'YES'), olac_train) +
+        nltk.util.LazyMap(lambda v: (fd(v), 'NO'), oai_train))
+    test_data = (
+        nltk.util.LazyMap(lambda v: (fd(v), 'YES'), olac_train) +
+        nltk.util.LazyMap(lambda v: (fd(v), 'NO'), oai_train))
+    
+    # Train a classifier
+    classifier = nltk.MaxentClassifier.train(train_data, ALGORITHM)
+    
+    # Display its accuracy
+    print 'Classifier accuracy:',
+    print nltk.classify.accuracy(classifier, test_data)
+    
+    # Display the most useful features
+    classifier.show_most_informative_features()
+            
+
+                   
+        
---
+++ src/classifier/tabdbreader.py
@@ -0,0 +1,97 @@
+"""
+Corpus readers for the Tab-delineated table format used by
+olacdb.tab and oaidb.tab.  The corpus reader scans through the
+file and collects all of the elements for a given item together,
+returning them as a single "record."  A record is a dictionary
+with two special keys (Archive_ID and Item_ID) identifying the
+record item, along with keys for the individual elements in the
+table.  E.g.:
+
+    >>> reader = TabTableCorpusReader('oai_classifier_trn', '.*db\.tab')
+    >>> olac_records = reader.records('olacdb.tab')
+    >>> print olac_records[0]
+    {'Archive_ID': '1',
+     'title': "Acquiring and Exploiting the User's Knowledge in Guidance Interactions",
+     'creator': 'Eyal Shifroni\nUzzi Ornan',
+     'contributor': 'Uzzi Ornan',
+     'Item_ID': '920998',
+     'identifier': 'http://www.aclweb.org/anthology/A92-1042'}
+
+If an element has more than one value, then the values are joined
+using newlines (e.g., the 'creator' element from the above example
+record has two values.)
+
+The set of elements that are used by the OAI database are:
+
+    'Archive_ID', 'description', 'format', 'contributor', 'creator',
+    'relation', 'coverage', 'date', 'subject', 'publisher',
+    'language', 'rights', 'title', 'source', 'Item_ID',
+    'identifier', 'type'
+
+The set of elements that are used by the OLAC database are:
+
+    'Archive_ID', 'conformsTo', 'extent', 'creator', 'abstract',
+    'isRequiredBy', 'relation', 'contributor', 'alternative',
+    'subject', 'bibliographicCitation', 'hasVersion', 'title',
+    'isFormatOf', 'source', 'spatial', 'isPartOf', 'type',
+    'available', 'medium', 'description', 'format', 'hasPart',
+    'tableOfContents', 'accessRights', 'Item_ID', 'coverage',
+    'date', 'isVersionOf', 'publisher', 'license', 'language',
+    'rights', 'created', 'modified', 'references', 'identifier',
+    'requires'
+
+"""
+
+from nltk.corpus.reader.util import *
+from nltk.corpus.reader.api import *
+
+class TabDBCorpusReader(CorpusReader):
+    def records(self, fileids=None):
+        if fileids is None: fileids = self._fileids
+        elif isinstance(fileids, basestring): fileids = [fileids]
+        return concat([TabDBCorpusView(fileid, encoding=enc)
+                       for (fileid, enc) in self.abspaths(fileids, True)])
+
+class TabDBCorpusView(StreamBackedCorpusView):
+    def read_block(self, stream):
+        if stream.tell() == 0:
+            # Read header.
+            assert stream.readline() == '--------------\n'
+            self.select_cmd = stream.readline()
+            assert self.select_cmd.startswith('select ')
+            assert stream.readline() == '--------------\n'
+            assert stream.readline() == '\n'
+            assert stream.readline() == (
+                'Archive_ID\tItem_ID\tElement_ID\tTagName\tType\t'
+                'Code\tContent\n')
+            self.fields = stream.readline().rstrip('\n').split('\t')
+            
+
+        record = {}
+        while True:
+            pos = stream.tell()
+            line = stream.readline()
+            if not line.strip(): break
+            fieldvals = line.rstrip('\n').split('\t')
+            (archive_id, item_id, elt_id, tag_name,
+                type, code, content) = fieldvals
+            if not record:
+                record['Archive_ID'] = archive_id
+                record['Item_ID'] = item_id
+            else:
+                if (record['Archive_ID'] != archive_id or
+                    record['Item_ID'] != item_id):
+                    stream.seek(pos)
+                    return [record]
+
+            # For fields with multipel values, use newlines to
+            # separate them.
+            if tag_name in record:
+                content = record[tag_name] + '\n' + content
+                
+            record[tag_name] = content
+
+        return [record]
+
+    def _mkrecord(self, line):
+        return dict(zip(self.fields, fieldvals))
