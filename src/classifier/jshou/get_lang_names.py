60f4e2c60376c927ed7f1b65167aa669966d4670
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-06-24 16:48:37 +0000

    This script takes an Ethnologue database dump and outputs a list of language names, both primary and alternate.
    git-svn-id: https://olac.googlecode.com/svn/@875 e9cfdc32-c5ba-11dd-9551-afe151041c70

---
+++ src/classifier/jshou/get_lang_names.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# get_lang_names.py
+# Joshua S Hou
+# June 2009
+#
+# This script takes as input an XML format Ethnologue database dump of languages
+# and its associated ISO code, primary and alternate names, and prints out a list
+# of every language name mentioned, primary or alternate.
+import xml.parsers.expat
+import sys
+
+class XMLParse:
+    def __init__(self, xml_file, output):
+        assert(xml_file != "")
+        self.xml_file = xml_file
+        self.output = open(output,'w')
+        self.path = []
+        self.Parser = xml.parsers.expat.ParserCreate()
+
+        self.Parser.CharacterDataHandler = self.handleCharData
+        self.Parser.StartElementHandler = self.handleStartElement
+        self.Parser.EndElementHandler = self.handleEndElement
+        
+        self.altlist = ''
+        self.lang_names = set()
+
+    def parse(self):
+            self.Parser.ParseFile(open(self.xml_file))
+
+    def handleCharData(self, data):
+        if self.path[-1]=="print_name":
+            self.lang_names.add(data.encode('utf-8'))
+        elif self.path[-1]=="alternate_names":
+            self.altlist += data.encode('utf-8')
+
+    def handleStartElement(self, name, attrs):
+        self.path.append(name)
+
+    def handleEndElement(self, name):
+        if name=="alternate_names":
+            for alt in self.altlist.split(", "):
+                self.lang_names.add(alt)
+            self.altlist = ''
+        self.path.pop()
+    
+    def print_lang_names(self):
+        for lang_name in self.lang_names:
+            print>>self.output, lang_name
+
+if __name__=="__main__":
+    if len(sys.argv)!=3:
+        print "Usage: python get_lang_names.py Ethnologue-classifier-training-data.xml lang_names"
+        sys.exit(1)
+    xp = XMLParse(sys.argv[1], sys.argv[2])
+    xp.parse()
+    xp.print_lang_names()
