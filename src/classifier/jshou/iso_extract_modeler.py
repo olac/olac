c5932275f60e2effc707b7fcba84be5ef5d9d979
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-06-29 15:52:39 +0000

    No longer calculates probabilities, but just keeps track of lists of isos per language name, country, and region.  Updated model printing method to reflect this change.  Added a method to read in a model from a textfile.  Commented up the code.
    git-svn-id: https://olac.googlecode.com/svn/@894 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/jshou/iso_extract_modeler.py
+++ src/classifier/jshou/iso_extract_modeler.py
@@ -3,13 +3,13 @@
 # Joshua S Hou
 # June 2009
 #
-# This module contains a class isoLangModel that manages the counts and probabilities
+# This module contains a class isoLangModel that manages the counts
 # of ISOs and associated primary and alternate language names from an Ethnologue database
-# dump to create a Naive Bayes model for classifying language names.  It depends on the
-# classes Lang, which is a simple wrapper class for holding a language name and a boolean
+# dump to create a model for classifying language names.  It depends on the
+# classes isoLang, which is a simple wrapper class for holding a language name, iso and a boolean
 # field indicating whether or not it is a primary language name, and EthnologueTrainingDataParser,
-# an XML parser that reads an Ethnologue database dump in XML format and stores counts and
-# probabilities in a given model file.
+# an XML parser that reads an Ethnologue database dump in XML format and stores counts 
+# in a given model file.
 
 import sys
 import re
@@ -19,7 +19,9 @@ from xml.parsers import expat
 
 class EthnologueTrainingDataParser:
     """Class for parsing Ethnologue training data.  It takes as an argument the name of the XML
-    file, as well as the model to be populated."""
+    file, as well as the model to be populated.  For each tag "alternate_names", "print_name",
+    "country_name" and "region", calls the model's methods for keeping track of iso codes associated
+    with each tag."""
     def __init__(self, xml_file, model):
         assert(xml_file != "")
         self.xml_file = xml_file
@@ -37,15 +39,13 @@ class EthnologueTrainingDataParser:
         self.tempcountry = ''
         self.tempregion = ''
         
+        self.spaces = re.compile(r'\s+')
+        
         self.curr_print_name = None
         self.curr_iso = None
 
     def parse(self):
-#        try:
-            self.Parser.ParseFile(open(self.xml_file))
-#        except:
-#            print "File", self.xml_file, "could not be opened."
-#            sys.exit(1)
+        self.Parser.ParseFile(open(self.xml_file))
 
     def handleCharData(self, data):
         if self.path[-1]=="print_name":
@@ -64,24 +64,24 @@ class EthnologueTrainingDataParser:
 
     def handleEndElement(self, name):
         if name=="alternate_names":
+            self.tempalt = self.spaces.sub(' ', self.tempalt)
             for alt in self.tempalt.split(', '):
                 if alt:
                     self.model.add_iso(alt, isoLang(self.curr_iso, alt, False))
-            self.altlist = ''
+            self.tempalt = ''
         elif name=="print_name":
-            self.curr_print_name = self.tempprim
+            self.curr_print_name = self.spaces.sub(' ',self.tempprim)
             self.tempprim = ''
         elif name=="ISO_code":
             self.curr_iso = self.tempiso
             self.tempiso = ''
             self.model.add_iso(self.curr_print_name, isoLang(self.curr_iso, self.curr_print_name, True))
         elif name=="country_name":
-            try:
-                self.model.c_iso_country[self.tempcountry].append(self.curr_iso)
-            except KeyError:
-                self.model.c_iso_country[self.tempcountry] = [self.curr_iso]
+            self.tempcountry = self.spaces.sub(' ', self.tempcountry)
+            self.model.add_country(self.tempcountry, self.curr_iso)
             self.tempcountry = ''
         elif name=="region":
+            self.tempregion = self.spaces.sub(' ', self.tempregion)
             self.model.add_region(self.curr_iso, self.tempregion)
             self.tempregion = ''
         self.path.pop() # presumably, the XML is well-formed and an end element is necessarily the closest preceding unclosed begin tag.
@@ -95,8 +95,10 @@ class isoLang:
         self.primary = primary
 
 class isoLangModel:
+    """Class that keeps track of ISO 639-3 codes associated with each country, region, and language name
+    as stored in the Ethnologue."""
     def __init__(self):
-        self.c_iso_lang = {} # Dictionary from lang to iso.  lang -> list of isos
+        self.c_iso_lang = {} # Dictionary from lang to iso.  lang -> list of isoLangss
         self.c_iso_country = {} # Dictionary from country -> list of isos
         self.c_iso_region = {} # Dictionary from region -> list of isos
         
@@ -104,17 +106,23 @@ class isoLangModel:
         self.region_NE = re.compile(r'([A-Z]\w*\s?)+')
     
     def train(self, trg_data_filename):
+        """Uses the EthnologueTrainingDataParser parse method to parse the XML Ethnologue training data."""
         etdp = EthnologueTrainingDataParser(trg_data_filename, self)
         etdp.parse()
     
     def add_iso(self, lang, iso_lang):
-        """iso_lang must be of type isoLang"""
+        """Adds iso_lang to language name lang.  iso_lang must be of type isoLang."""
+        lang = lang.strip('"')
+        if "," in lang:
+            splitname = lang.split(',')
+            lang = (splitname[1]+' '+splitname[0]).strip()
         try:
             self.c_iso_lang[lang].append(iso_lang)
         except KeyError:
             self.c_iso_lang[lang] = [iso_lang]
     
     def add_region(self, iso, region_string):
+        """Adds iso to region."""
         region_NEs = self.region_extract(region_string)
         for ne in region_NEs:
             try:
@@ -122,12 +130,22 @@ class isoLangModel:
             except KeyError:
                 self.c_iso_region[ne] = [iso]
     
+    def add_country(self, country, iso):
+        """Adds iso to country."""
+        try:
+            self.c_iso_country[country].append(iso)
+        except KeyError:
+            self.c_iso_country[country] = [iso]
+    
     def region_extract(self, region_string):
         """Returns a list of region named entities, where region NEs are defined
         as consecutive capitalized words."""
         return map(string.strip, self.region_NE.findall(region_string))
     
     def calc_probs(self):
+        """Calculates probability of each iso given a language name and stores the
+        results in a dictionary.  This method is currently not being used, because
+        we are not calculating probabilities for now."""
         for lang in self.c_iso_lang:
             self.p_iso_lang[lang] = {}
             p_iso = self.p_iso_lang[lang]
@@ -142,9 +160,33 @@ class isoLangModel:
                 p_iso[isoLang.iso] /= denom
     
     def print_model(self, outstream):
-        for lang in self.p_iso_lang:
-            for iso in self.p_iso_lang[lang]:
-                print>>outstream, iso, lang, self.p_iso_lang[lang][iso]
+        """Prints out the list of isos associated with each country, language name, and region stored
+        in the model."""
+        for country in self.c_iso_country:
+            print>>outstream, "Country"+'\t'+country+'\t'+' '.join(self.c_iso_country[country])
+        for lang in self.c_iso_lang:
+            print>>outstream, "Language_name"+'\t'+lang+'\t'+' '.join([x.iso for x in self.c_iso_lang[lang]])
+        for region in self.c_iso_region:
+            print>>outstream, "Region"+'\t'+region+'\t'+' '.join(self.c_iso_region[region])
+#        this code below is the function for printing part of the old model with probabilities
+#        for lang in self.p_iso_lang:
+#            for iso in self.p_iso_lang[lang]:
+#                print>>outstream, iso, lang, self.p_iso_lang[lang][iso]
+    def read_model(self, modelfilename):
+        """Clears whatever is currently stored in the model and reads in a model from file."""
+        self.c_iso_lang = {} # Dictionary from lang to iso.  lang -> list of isoLangss
+        self.c_iso_country = {} # Dictionary from country -> list of isos
+        self.c_iso_region = {} # Dictionary from region -> list of isos
+        
+        modelfile = open(modelfilename).readlines()
+        for line in modelfile:
+            model_line = line.strip().split('\t')
+            if model_line[0]=="Country":
+                self.c_iso_country[model_line[1]] = model_line[2].split()
+            elif model_line[0]=="Language_name":
+                self.c_iso_lang[model_line[1]] = model_line[2].split()
+            elif model_line[0]=="Region":
+                self.c_iso_region[model_line[1]] = model_line[2].split()
    
 if __name__=="__main__":
     if len(sys.argv)!=4:
@@ -153,7 +195,7 @@ if __name__=="__main__":
         sys.exit(1)
     ilm = isoLangModel()
     ilm.train(sys.argv[1])
-    ilm.calc_probs()
+#    ilm.calc_probs()
     if sys.argv[2]=="0":
         ilm.print_model(open(sys.argv[3],'w'))
     else:
