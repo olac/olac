4d3853420915025ec97e57a2ba92d72cd947c4fb
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-06-30 15:47:38 +0000

    Added a debug mode.
    git-svn-id: https://olac.googlecode.com/svn/@904 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/jshou/iso_extractor.py
+++ src/classifier/jshou/iso_extractor.py
@@ -14,7 +14,7 @@ class isoClassifier:
     def __init__(self, model_file):
         self.model = pickle.load(open(model_file,'rb'))
     
-    def classify_record(self, record):
+    def classify_record(self, record, debug):
         """This method goes through the different fields of a record and gets the language names."""
         results = {}
         langs = []
@@ -31,21 +31,30 @@ class isoClassifier:
             bag_of_words += ' '+record['description']
         except KeyError:
             pass
-        
+# DEBUG MODE 
+        if debug:
+            print record['Archive_ID'], record['Item_ID'], record['title']
+
         iso_set = set()
         for lang_name in self.model.c_iso_lang:
             if lang_name in bag_of_words:
                 iso_set.update(self.model.c_iso_lang[lang_name])
+                if debug:
+                    print "lang_name:", lang_name, self.model.c_iso_lang[lang_name]
         
         country_iso_set = set()
         for country_name in self.model.c_iso_country:
             if country_name in bag_of_words:
                 country_iso_set.update(self.model.c_iso_country[country_name])
+                if debug:
+                    print "country name:", country_name, self.model.c_iso_country[country_name]
         
         region_iso_set = set()
         for region in self.model.c_iso_region:
             if region in bag_of_words:
                 region_iso_set.update(self.model.c_iso_region[region])
+                if debug:
+                    print "region:", region, self.model.c_iso_region[region]
         
         country_intersection = iso_set.intersection(country_iso_set)
         if country_intersection:
@@ -54,11 +63,13 @@ class isoClassifier:
         if region_intersection:
             iso_set = region_intersection
         # title, subject, description
+        if debug:
+            print "final iso set:", iso_set
         return iso_set
 
 if __name__=="__main__":
     if len(sys.argv)!=4:
-        print "Usage: python iso_extractor.py model.pkl tabfile outfile"
+        print "Usage: python iso_extractor.py model.pkl debug[0 or 1] outfile"
         sys.exit(1)
     
     ic = isoClassifier(sys.argv[1])
@@ -67,8 +78,12 @@ if __name__=="__main__":
     olac_records = reader.records('olacdb.tab')
     
     outfile = open(sys.argv[3],'w') 
+    
+    debug = sys.argv[2]=="1"
+    if debug:
+        olac_records = olac_records[:100]
     for record in olac_records:
-        results = ic.classify_record(record)
+        results = ic.classify_record(record, debug)
         try:
             title = record['title']
         except KeyError:
