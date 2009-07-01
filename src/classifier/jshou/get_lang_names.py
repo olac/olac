9f87f54d68323f63dc27f86ad666c0a5bfb269ee
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-07-01 16:26:33 +0000

    Prints to stdout the language names that start with a small letter, for debugging purposes.
    git-svn-id: https://olac.googlecode.com/svn/@908 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/jshou/get_lang_names.py
+++ src/classifier/jshou/get_lang_names.py
@@ -8,6 +8,7 @@
 # of every language name mentioned, primary or alternate.
 import xml.parsers.expat
 import sys
+import re
 
 class XMLParse:
     def __init__(self, xml_file, output):
@@ -24,39 +25,52 @@ class XMLParse:
         self.primlist = ''
         self.altlist = ''
         self.lang_names = set()
+        self.curr_iso = ''
+        self.spaces = re.compile(r'\s+')
 
     def parse(self):
             self.Parser.ParseFile(open(self.xml_file))
 
     def handleCharData(self, data):
         if self.path[-1]=="print_name":
-            self.primlist += " "+data.encode('utf-8').strip()
+            self.primlist += data.encode('utf-8')
         elif self.path[-1]=="alternate_names":
-            self.altlist += " "+data.encode('utf-8').strip()
+            self.altlist += data.encode('utf-8')
+        elif self.path[-1]=="ISO_code":
+            self.curr_iso = data.strip()
 
     def handleStartElement(self, name, attrs):
         self.path.append(name)
 
     def handleEndElement(self, name):
         if name=="alternate_names":
-#            self.lang_names.add(self.altlist.encode('utf-8'))
+            self.altlist = self.spaces.sub(' ', self.altlist)
             for alt in self.altlist.split(", "):
-                self.lang_names.add(alt.strip('" '))
+                if alt:
+                    self.lang_names.add(alt.strip('" '))
+                    # the next two lines are looking for little letters at the beginning of a name
+                    if alt[0].islower():
+                        print self.curr_iso+":", alt
             self.altlist = ''
         elif name=="print_name":
-            print_name = self.primlist.strip('" ')
+            print_name = self.spaces.sub(' ',self.primlist).strip('" ')
             if "," in print_name:
-                splitname = print_name
-                print_name = (splitname[1]+" "+splitname[0]).strip()
-            self.lang_names.add(print_name)
+                splitname = print_name.split(',')
+                inverted_name = (splitname[1]+" "+splitname[0]).strip()
+                self.lang_names.add(inverted_name)
+                if inverted_name[0].islower():
+                    print self.curr_iso+", primary uninverted:", print_name, inverted_name
+            else:
+                if print_name[0].islower():
+                    print self.curr_iso+", primary untouched:", print_name
+                self.lang_names.add(print_name)
             
             self.primlist = ''
         self.path.pop()
     
     def print_lang_names(self):
         for lang_name in self.lang_names:
-            if lang_name:
-                print>>self.output, lang_name
+            print>>self.output, lang_name
 
 if __name__=="__main__":
     if len(sys.argv)!=3:
