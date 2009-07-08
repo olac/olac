9fd3041791d5d0232011741bc156242898dc0966
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-07-08 14:54:23 +0000

    Using optparse to handle arguments.  Now takes .tab datafile from any directory specified.
    git-svn-id: https://olac.googlecode.com/svn/@960 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/jshou/iso639Classifier.py
+++ src/classifier/jshou/iso639Classifier.py
@@ -1,4 +1,9 @@
-'''Reads OLAC records from olacdb.tab and prints out hypothesized ISO 639 language codes
+'''Reads OLAC records from olacdb2.tab and prints out hypothesized ISO 639 language codes
+
+Uses trained pickled classifier created with iso639_trainer.py to classify OLAC
+records from a tab delimited file of records.  Debug mode prints out the
+language, country and region names that the classifier recognizes.  Prints out
+OAI Identifier, hypothesized ISO 639 language codes, and titles of each record.
 
 Created on Jul 6, 2009
 
@@ -11,24 +16,32 @@ iso639Classifier -- Reads olacdb2.tab data and prints possible ISO 639 language 
 
 import sys
 import pickle
+from optparse import OptionParser
 from tabdbreader2 import *
 from iso639_trainer import *
+
+# Parses arguments and options
+parser = OptionParser(usage='python iso639Classifier.py [options] classifier.pickle input > classifier-output')
+parser.add_option('-d', '--debug', action='store_true', dest='debug', help='Prints out the '+\
+                  'language, country and region names that the classifier recognizes.')
+parser.add_option('-n', '--num', type='int', dest='num', default=0)
+(options, args) = parser.parse_args()
     
-if len(sys.argv)<3:
-    print "Usage: python iso639Classifier.py classifier.pickle input 1 > classifier-output"
-    print len(sys.argv)
+if len(args)<2:
+    parser.print_help()
     sys.exit(1)
 
-classifier = pickle.load(open(sys.argv[1],'rb'))
-if len(sys.argv)>3 and sys.argv[3]=="1":
-    classifier.debug=True
+# Loads classifier and initializes corpus reader
+classifier = pickle.load(open(args[0],'rb'))
+classifier.debug = options.debug
 
-reader = TabDBCorpusReader('../oai_classifier_trn', '.*db\.tab')
-olac_records = reader.records(sys.argv[2])
+reader = TabDBCorpusReader('.', '.*db\.tab')
+olac_records = reader.records(args[1])
 
-#print>>sys.stderr, "Number of olac records:", len(olac_records)
-
-for record in olac_records[-5000:]:
+# Classifies each record
+if options.num:
+    olac_records = olac_records[:options.num]
+for record in olac_records:
     bag_of_words = ''
     try:
         bag_of_words += record['title']
