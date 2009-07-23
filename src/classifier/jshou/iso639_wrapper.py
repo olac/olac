9d938ff1f23f77f66e886630fb4f59e5b77f2f0b
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-07-23 21:23:17 +0000

    A wrapper that calls all the modules for training and testing the classifier.  For debugging purposes only.
    git-svn-id: https://olac.googlecode.com/svn/@1022 e9cfdc32-c5ba-11dd-9551-afe151041c70

---
+++ src/classifier/jshou/iso639_wrapper.py
@@ -0,0 +1,42 @@
+'''Wrapper that calls iso 639 trainer, classifier, and evaluator.
+
+Wrapper that calls iso639_trainer.py, iso639Classifier.py and iso639_evaluator
+all in one go.  It is not scalable, but is meant for running experiments,
+because it can call everything at once.
+
+Created on Jul 9, 2009
+
+@author: jshou
+'''
+import sys
+import os
+import optparse
+import classifier_functions
+
+parser = optparse.OptionParser()
+parser.add_option('-t', '--ten', action='store_true', dest='ten', help='Run classifier over all 10 functions and print the performance results')
+(options, args) = parser.parse_args()
+
+if options.ten:
+    for i in range(1,10):
+        print classifier_functions.function_labels[i-1]+'\n'
+        print "--CLASSIFYING TEST--"
+        os.system('python iso639Classifier.py -d -f -i %d classifier.pickle iso639_test2.tab iso639_test2.classified' % i-1)
+        print "--EVALUATING TEST--"
+        os.system('python iso639_evaluator.py iso639_test2.gs iso639_test2.classified')
+        print "--CLASSIFYING OLAC DISPLAY--"
+        os.system('python iso639Classifier.py -d -f -i %d classifier.pickle olac_display_subset.xml olac_display_subset.classified' % i-1)
+        print "--EVALUATING OLAC DISPLAY--"
+        os.system('python iso639_evaluator.py olac_display_subset.gs olac_display_subset.classified')
+        print "\n--------------------------------------------------------------------\n"
+else:
+    print "--TRAINING--"
+    os.system('python iso639_trainer.py -f preprocessor/training_data.tab classifier.pickle')
+    print "--CLASSIFYING TEST--"
+    os.system('python iso639Classifier.py -d -f classifier.pickle iso639_test2.tab iso639_test2.classified')
+    print "--EVALUATING TEST--"
+    os.system('python iso639_evaluator.py iso639_test2.gs iso639_test2.classified')
+    print "--CLASSIFYING OLAC DISPLAY--"
+    os.system('python iso639Classifier.py -d -f classifier.pickle olac_display_subset.xml olac_display_subset.classified')
+    print "--EVALUATING OLAC DISPLAY--"
+    os.system('python iso639_evaluator.py olac_display_subset.gs olac_display_subset.classified')
