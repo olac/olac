6f64b514cd1bbd24a86611cb76722ec2af0160dd
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-07-27 13:49:23 +0000

    changed to handle other xml files
    git-svn-id: https://olac.googlecode.com/svn/@1034 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/jshou/create_xml_goldstandard.py
+++ src/classifier/jshou/create_xml_goldstandard.py
@@ -6,10 +6,16 @@ import re
 from xmlreader import *
 from util import *
 
+if len(sys.argv)!=3:
+    sys.exit("Usage: python create_xml_goldstandard.py xmlfile goldstandard")
+
+xmlfilename = sys.argv[1]
+gs_filename = sys.argv[2]
+
 reader = XMLCorpusReader('.', '.*\.xml')
-c = reader.records('olac_display_subset.xml')
+c = reader.records(xmlfilename)
 newline = re.compile(r'\n')
-gs = check_file('olac_display_subset.gs','wb',utf=True)
+gs = check_file(gs_filename,'wb',utf=True)
 
 for record in c:
     if 'iso639' in record:
