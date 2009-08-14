afc281c6bff3c2baf50445f2935a680bf234f612
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-08-14 20:34:06 +0000

    uses olac_subset_display2 instead
    git-svn-id: https://olac.googlecode.com/svn/@1071 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/jshou/preprocessor/preprocess.py
+++ src/classifier/jshou/preprocessor/preprocess.py
@@ -16,7 +16,12 @@ if '/' in sys.argv[0]:
     header = sys.argv[0].rsplit('/')[0]+'/'
 else:
     header = ''
-os.system('python %(header)sEthnologue_data_preprocessor.py %(header)sEthnologue-classifier-training-data.xml > %(output)s' % {'header':header, 'output':sys.argv[1]})
-os.system('python %(header)sLinguistList_preprocessor.py %(header)sGetListOfAncientLgs.html >> %(output)s' % {'header':header, 'output':sys.argv[1]})
-os.system('python %(header)siso-639_preprocessor.py %(header)siso-639-3_20090210.tab.dld >> %(output)s' % {'header':header, 'output':sys.argv[1]})
-os.system('cat %(header)sextra_data.tab >> %(output)s' % {'header':header, 'output':sys.argv[1]})
+headout = {'header':header, 'output':sys.argv[1]}
+os.system('python %(header)sEthnologue_data_preprocessor.py %(header)sEthnologue-classifier-training-data.xml > %(output)s' % headout)
+os.system('python %(header)sLinguistList_preprocessor.py %(header)sGetListOfAncientLgs.html >> %(output)s' % headout)
+os.system('python %(header)siso-639_preprocessor.py %(header)siso-639-3_20090210.tab.dld >> %(output)s' % headout)
+os.system('python %(header)sCountryCodes_preprocessor.py %(header)sCountryCodes.tab >>%(output)s' % headout)
+output = open(headout['output'],'a')
+print>>output, open(headout['header']+'extra_data.tab').read().rstrip('\n\r')
+print>>output, open(headout['header']+'Data for language classifier.tab').read().rstrip('\n\r')
+print>>output, open(headout['header']+'LCSH_names_trainingdata.tab').read().rstrip('\n\r')
