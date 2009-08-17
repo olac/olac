1f65f1b844ef926e11fde197ecf3440ba5ce2142
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-08-17 18:27:39 +0000

    using os.path so that it works in windows too
    git-svn-id: https://olac.googlecode.com/svn/@1077 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/jshou/preprocessor/preprocess.py
+++ src/classifier/jshou/preprocessor/preprocess.py
@@ -12,11 +12,8 @@ if os.path.exists(sys.argv[1]):
     else:
         print "Overwriting..."
 
-if '/' in sys.argv[0]:
-    header = sys.argv[0].rsplit('/')[0]+'/'
-else:
-    header = ''
-headout = {'header':header, 'output':sys.argv[1]}
+header, file = os.split(sys.argv[0])
+headout = {'header':os.path.join(header,''), 'output':sys.argv[1]}
 os.system('python %(header)sEthnologue_data_preprocessor.py %(header)sEthnologue-classifier-training-data.xml > %(output)s' % headout)
 os.system('python %(header)sLinguistList_preprocessor.py %(header)sGetListOfAncientLgs.html >> %(output)s' % headout)
 os.system('python %(header)siso-639_preprocessor.py %(header)siso-639-3_20090210.tab.dld >> %(output)s' % headout)
@@ -25,3 +22,4 @@ output = open(headout['output'],'a')
 print>>output, open(headout['header']+'extra_data.tab').read().rstrip('\n\r')
 print>>output, open(headout['header']+'Data for language classifier.tab').read().rstrip('\n\r')
 print>>output, open(headout['header']+'LCSH_names_trainingdata.tab').read().rstrip('\n\r')
+print>>output, open(headout['header']+'ancient_countries.tab').read().rstrip('\n\r')
