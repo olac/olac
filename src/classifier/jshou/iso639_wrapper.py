b65a9b1227afd4cb21bb2807ae10c82b3c7f7eb6
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-08-19 17:55:28 +0000

    Now expects test data to be in test_data directory
    git-svn-id: https://olac.googlecode.com/svn/@1088 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/jshou/iso639_wrapper.py
+++ src/classifier/jshou/iso639_wrapper.py
@@ -16,7 +16,7 @@ from util import *
 
 parser = optparse.OptionParser()
 parser.add_option('-t', '--ten', action='store_true', dest='ten', help='Run classifier over all 10 functions and print the performance results')
-parser.add_option('-f', '--file', dest='file', default='iso639_test2.tab', help='Specifies name of file to be classified.')
+parser.add_option('-f', '--file', dest='file', default='test_data/iso639_test2.tab', help='Specifies name of file to be classified.')
 (options, args) = parser.parse_args()
 
 header = options.file.split('.')[0]
@@ -32,10 +32,10 @@ if options.ten:
         os.system('python iso639_evaluator.py %s.gs %s.classified' % (header, header))
         print "--CLASSIFYING OLAC DISPLAY--"
         sys.stdout.flush()
-        os.system('python iso639Classifier.py -g olac_display_subset2.gs -d -f -i %d classifier.pickle olac_display_subset2.xml olac_display_subset2.classified' % i)
+        os.system('python iso639Classifier.py -g test_data/olac_display_subset2.gs -d -f -i %d classifier.pickle test_data/olac_display_subset2.xml test_data/olac_display_subset2.classified' % i)
         print "--EVALUATING OLAC DISPLAY--"
         sys.stdout.flush()
-        os.system('python iso639_evaluator.py olac_display_subset2.gs olac_display_subset2.classified')
+        os.system('python iso639_evaluator.py test_data/olac_display_subset2.gs test_data/olac_display_subset2.classified')
         print "\n--------------------------------------------------------------------\n"
 else:
     print "--TRAINING--"
@@ -49,7 +49,7 @@ else:
     os.system('python iso639_evaluator.py %s.gs %s.classified' % (header, header))
     print "--CLASSIFYING OLAC DISPLAY--"
     sys.stdout.flush()
-    os.system('python iso639Classifier.py -g olac_display_subset2.gs -d -f classifier.pickle olac_display_subset2.xml olac_display_subset2.classified')
+    os.system('python iso639Classifier.py -g test_data/olac_display_subset2.gs -d -f classifier.pickle test_data/olac_display_subset2.xml test_data/olac_display_subset2.classified')
     print "--EVALUATING OLAC DISPLAY--"
     sys.stdout.flush()
-    os.system('python iso639_evaluator.py olac_display_subset2.gs olac_display_subset2.classified')
+    os.system('python iso639_evaluator.py test_data/olac_display_subset2.gs test_data/olac_display_subset2.classified')
