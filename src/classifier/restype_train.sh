555863d6875598c8f7fdba7f9779d05a5bf1fd5b
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-08-28 14:27:56 +0000

    factored out setting of mallet environment variables
    git-svn-id: https://olac.googlecode.com/svn/@1133 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/restype_train.sh
+++ src/classifier/restype_train.sh
@@ -8,8 +8,6 @@ then
     exit $E_BADARGS
 fi
 
-export PATH=${PATH}:/usr/share/mallet-2.0-RC4/bin
-export CLASSPATH=.:/usr/share/mallet-2.0-RC4/src:/usr/share/mallet-2.0-RC4/class:/usr/share/mallet-2.0-RC4/lib/trove-2.0.2.jar:/usr/share/mallet-2.0-RC4/lib/bsh.jar
-
+./set_mallet_path.sh
 python malletize.py $1 $2
 mallet train-classifier --input $2 --output-classifier $3 --trainer MaxEnt
---
+++ src/classifier/set_mallet_path.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PATH=${PATH}:/usr/share/mallet-2.0-RC4/bin
+export CLASSPATH=.:/usr/share/mallet-2.0-RC4/src:/usr/share/mallet-2.0-RC4/class:/usr/share/mallet-2.0-RC4/lib/trove-2.0.2.jar:/usr/share/mallet-2.0-RC4/lib/bsh.jar
