e4902c6b70cd0089c9f08d728657bd25b3978049
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-09-03 20:50:53 +0000

    handles multiple class labels, and none
    git-svn-id: https://olac.googlecode.com/svn/@1156 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/restype_evaluate.py
+++ src/classifier/restype_evaluate.py
@@ -12,16 +12,29 @@ classifier's labels.
 Usage: python restype_evaluate.py classifier-output"
 '''
 import sys
+import operator
 
 if len(sys.argv)!=2:
     sys.exit("Usage: python restype_evaluate.py classifier-output")
 
+# none labels: language none, and none
+nones = set(['LNONE','NONE'])
+
 correct = 0
 input = open(sys.argv[1]).readlines()
 for line in input:
-    name, true_label, data = line.strip().split('\t')
-    assigned_label = data.split()[0].split(':')[0]
-    if true_label.strip()==assigned_label:
-        correct += 1
+    name, true_labels, data = line.strip().split('\t')
+    assigned_labels = data.split()[0].split(':')[0]
+    true_labels = set(true_labels.split('&&'))
+    if len(true_labels)>1:
+        # if multilabel, true_labels must be a subset of assigned_labels
+        if reduce(operator.iand,[label in true_labels for label in assigned_labels.split('&&')]):
+            correct += 1
+    else:
+        # if one label, must be exact match
+        if assigned_labels in true_labels:
+            correct += 1
+        elif assigned_labels in nones and true_labels.issubset(nones):
+            correct += 1
 
 print "Accuracy:", float(correct)/len(input)
