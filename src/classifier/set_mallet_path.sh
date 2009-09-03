3f5dd1f0eba90befc02fcae9755cc5560eb0572a
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-09-03 16:48:59 +0000

    changed so that MALLET path is in one variable
    git-svn-id: https://olac.googlecode.com/svn/@1155 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/set_mallet_path.sh
+++ src/classifier/set_mallet_path.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-export PATH=${PATH}:/usr/share/mallet-2.0-RC4/bin
-export CLASSPATH=.:/usr/share/mallet-2.0-RC4/src:/usr/share/mallet-2.0-RC4/class:/usr/share/mallet-2.0-RC4/lib/trove-2.0.2.jar:/usr/share/mallet-2.0-RC4/lib/bsh.jar
+# Sets Java CLASSPATH to include MALLET class and src directories, as well as
+# two jar files that the Resource Type Classifier is dependent on.  Please set
+# variable MALLET_PATH to path to MALLET directory.
+MALLET_PATH=/usr/share/mallet-2.0-RC4
+export PATH=${PATH}:${MALLET_PATH}/bin
+export CLASSPATH=.:${MALLET_PATH}/src:${MALLET_PATH}/class:${MALLET_PATH}/lib/trove-2.0.2.jar:${MALLET_PATH}/lib/bsh.jar
