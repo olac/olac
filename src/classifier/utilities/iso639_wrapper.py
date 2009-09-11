5eff257d9a770097a99e39ad03f25fbd979db4fd
joshuahou <joshuahou@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-09-11 20:40:00 +0000

    training from a directory now
    git-svn-id: https://olac.googlecode.com/svn/@1180 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/classifier/utilities/iso639_wrapper.py
+++ src/classifier/utilities/iso639_wrapper.py
@@ -18,16 +18,16 @@ dir_dict = {"directory":basedir}
 
 print "--TRAINING--"
 sys.stdout.flush()
-os.system('python %(directory)s/iso639_trainer.py -f %(directory)s/preprocessor/training_data.tab %(directory)s/classifier.pickle'%dir_dict)
+os.system('python %(directory)s/iso639_trainer.py -s %(directory)s/stoplist.txt -f %(directory)s/subjectlanguagedata %(directory)s/classifier.pickle'%dir_dict)
 print "--CLASSIFYING TEST--"
 sys.stdout.flush()
 os.system('python %(directory)s/iso639Classifier.py -g %(directory)s/test_data/iso639_test2.gs -d -f %(directory)s/classifier.pickle %(directory)s/test_data/iso639_test2.tab %(directory)s/test_data/iso639_test2.classified'%dir_dict)
 print "--EVALUATING TEST--"
 sys.stdout.flush()
 os.system('python %(directory)s/utilities/iso639_evaluator.py %(directory)s/test_data/iso639_test2.gs %(directory)s/test_data/iso639_test2.classified'%dir_dict)
-print "--CLASSIFYING OLAC DISPLAY--"
-sys.stdout.flush()
-os.system('python %(directory)s/iso639Classifier.py -g %(directory)s/test_data/olac_display_subset2.gs -d -f %(directory)s/classifier.pickle %(directory)s/test_data/olac_display_subset2.xml %(directory)s/test_data/olac_display_subset2.classified'%dir_dict)
-print "--EVALUATING OLAC DISPLAY--"
-sys.stdout.flush()
-os.system('python %(directory)s/utilities/iso639_evaluator.py %(directory)s/test_data/olac_display_subset2.gs %(directory)s/test_data/olac_display_subset2.classified'%dir_dict)
+#print "--CLASSIFYING OLAC DISPLAY--"
+#sys.stdout.flush()
+#os.system('python %(directory)s/iso639Classifier.py -g %(directory)s/test_data/olac_display_subset2.gs -d -f %(directory)s/classifier.pickle %(directory)s/test_data/olac_display_subset2.xml %(directory)s/test_data/olac_display_subset2.classified'%dir_dict)
+#print "--EVALUATING OLAC DISPLAY--"
+#sys.stdout.flush()
+#os.system('python %(directory)s/utilities/iso639_evaluator.py %(directory)s/test_data/olac_display_subset2.gs %(directory)s/test_data/olac_display_subset2.classified'%dir_dict)
