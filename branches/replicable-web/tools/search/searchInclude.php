5db83fb2d8644f5272525f5a1e54cab6dc625a51
haepal <haepal@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2009-10-02 22:04:14 +0000

    - made a backup copy of current web with tag "web_old_server_before_oct_09" - removed branches folder since it's of no use
    Update issue 12
    
    
    
    git-svn-id: https://olac.googlecode.com/svn/@1249 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- branches/replicable-web/tools/search/searchInclude.php
+++
@@ -1,6 +0,0 @@
-<?php
-#$URLBASE = "http://www.ldc.upenn.edu/olac/";
-$URLBASE = 'http://'.$_SERVER[SERVER_NAME].'/'.dirname($_SERVER[PHP_SELF]).'/';
-define('LIB', "./lib/");
-$STYLE = "./style/";
-?>
---
+++ tags/web_old_server_before_oct_09/tools/harvester/cleanup.py
@@ -0,0 +1,106 @@
+import os
+import sys
+import MySQLdb
+import datetime
+from optionparser import OptionParser
+
+def remove_unregistered_archives():
+    sql = "select oa.Archive_ID from OLAC_ARCHIVE oa " \
+          "left join ARCHIVES a on oa.RepositoryIdentifier=a.ID " \
+          "and oa.BaseURL=a.BaseURL where a.ID is null"
+    cur.execute(sql)
+    for aid, in cur.fetchall():
+        sql = "delete ai.*, me.* from ARCHIVED_ITEM ai, METADATA_ELEM me " \
+              "where ai.Archive_ID=%s and ai.Item_ID=me.Item_ID"
+        cur.execute(sql, aid)
+        sql = "delete from ARCHIVE_PARTICIPANT where Archive_ID=%s"
+        cur.execute(sql, aid)
+        sql = "delete from OLAC_ARCHIVE where Archive_ID=%s"
+        cur.execute(sql, aid)
+
+def remove_archives_with_no_baseurl():
+    sql = "delete oa.*, ai.*, me.* " \
+          "from OLAC_ARCHIVE oa, ARCHIVED_ITEM ai, METADATA_ELEM me " \
+          "where (oa.BaseURL='' or oa.BaseURL is null) and " \
+          " ai.Archive_ID=oa.Archive_ID and ai.Item_ID=me.Item_ID"
+    cur.execute(sql)
+
+def remove_records_with_no_archiveid():
+    sql = "delete ai.*, me.* " \
+          "from ARCHIVED_ITEM ai, METADATA_ELEM me " \
+          "where ai.Archive_ID is null and ai.Item_ID=me.Item_ID"
+    cur.execute(sql)
+    
+def remove_redundant_records():
+    sql = "delete ai.*, me.* from METADATA_ELEM me, ARCHIVED_ITEM ai, (%s) x " \
+          "where ai.OaiIdentifier=x.OaiIdentifier and ai.%s < m " \
+          "and ai.Item_ID=me.Item_ID"
+    subsql = "select OaiIdentifier, max(%s) m from ARCHIVED_ITEM " \
+             "group by OaiIdentifier having count(*) > 1"
+
+    for f in ("DateStamp", "Item_ID"):
+        cur.execute(sql % (subsql % f, f))
+
+def remove_empty_elements():
+    sql = "delete from METADATA_ELEM where (Content is null or Content='') and (Code is null or Code='')"
+    cur.execute(sql)
+
+
+if __name__ == "__main__":
+    usageString = """\
+Usage: %(prog)s [-h] -c <mycnf> [-H <host>] [-d <db>]
+
+    options:
+
+      -h          print this message and exit
+      -c <mycnf>  mycnf file
+      -H <host>   hostname of the mysql server
+      -d <db>     name of the olac database
+
+""" % {"prog":os.path.basename(sys.argv[0])}
+    
+    def usage(msg=None):
+        print >>sys.stderr, usageString
+        if msg:
+            print >>sys.stderr, "ERROR:", msg
+            print >>sys.stderr
+        sys.exit(1)
+        
+    op = OptionParser(
+        "*-h",
+        "-c:",
+        "*-H:",
+        "*-d:",
+        )
+    try:
+        op.parse(sys.argv[1:])
+    except OptionParser.ParseError, e:
+        usage(e.message)
+    if op.get('-h'): usage()
+    
+    mycnf = op.getOne('-c')
+    host = op.getOne('-H')
+    db = op.getOne('-d')
+
+    opts = {"read_default_file":mycnf}
+    if host: opts["host"] = host
+    if db: opts["db"] = db
+    con = MySQLdb.connect(**opts)
+    cur = con.cursor()
+
+    t = lambda: datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%d]")
+    print t(), "Starting database cleanup."
+    print t(), "Removing unregistered archives..."
+    remove_unregistered_archives()
+    print t(), "Removing archives with no BaseURL..."
+    remove_archives_with_no_baseurl()
+    print t(), "Removing records with no Archive_ID..."
+    remove_records_with_no_archiveid()
+    print t(), "Removing redundant records..."
+    remove_redundant_records()
+    print t(), "Removing empty elements..."
+    remove_empty_elements()
+    print t(), "Database cleanup finished."
+    con.commit()
+    cur.close()
+    con.close()
