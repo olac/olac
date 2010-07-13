24c2a847ed24e439ab506769e4f279665bc1e289
sven.pedersen <sven.pedersen@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2010-07-13 20:31:58 +0000

    Add new classifier wrapper with binary and multi-way (type) classifier "servers" that load once to allow for faster processing.
    git-svn-id: https://olac.googlecode.com/svn/@1468 e9cfdc32-c5ba-11dd-9551-afe151041c70

---
+++ src/sandbox/oai-harvester-classifier/classifier/multi/server.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# server.sh
+# Apr 28, 2010
+# Sven Pedersen for Classification "server"
+# 
+# Test stage of resource type classifier
+# 
+# Usage: ./server.sh
+
+EXPECTED_ARGS=0
+E_BADARGS=65
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "Usage: `basename $0`"
+    echo "  input-document must be in plaintext format:"
+    echo "  [name]\t[label]\t[data]"
+    exit $E_BADARGS
+fi
+
+# set path and java classpath environment variables for mallet
+source ../set_mallet_path.sh
+
+# ensure compiled version of ResourceTypeClassify exists
+if [ ! -e "ResourceTypeClassifierServer.class" ]
+then
+    echo "No binary for ResourceTypeClassifierServer found."
+    echo "Compiling..."
+    javac ResourceTypeClassifierServer.java
+fi
+
+process="MalletClassifierInputFile.tmp"
+output="MalletClassifierOutputFile.tmp"
+classifierFile="../resourceTypeMultiClassifier.mallet"
+
+echo "Starting classifier server..."
+#RESULT=$(java ResourceTypeClassifierServer $classifierFile)
+java -Xms18m -Xmx128m ResourceTypeClassifierServer $classifierFile &
+
+
