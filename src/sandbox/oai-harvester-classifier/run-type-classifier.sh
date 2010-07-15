112e6fe41c9aeb86dec28c7ed4c33b9d18f6eca7
sven.pedersen <sven.pedersen@e9cfdc32-c5ba-11dd-9551-afe151041c70>
2010-07-15 13:58:11 +0000

    scrub imported unicode data, import binary classification probabilities, remove carriage returns
    git-svn-id: https://olac.googlecode.com/svn/@1478 e9cfdc32-c5ba-11dd-9551-afe151041c70

--- src/sandbox/oai-harvester-classifier/run-type-classifier.sh
+++ src/sandbox/oai-harvester-classifier/run-type-classifier.sh
@@ -1,5 +1,7 @@
 # setup temp files
 P=/usr/share/olac/olac/src/sandbox/oai-harvester-classifier
+TMPA=$P/restype.outA.tmp # raw source data
+TMP0=$P/restype.out0.tmp # clean source data
 TMP1=$P/restype.out1.tmp # binary classifier input
 TMP2=$P/restype.out2.tmp # binary classifier output
 TMP3=$P/restype.out3.tmp # type classifier input
@@ -8,9 +10,17 @@ TMP5=$P/restype.out5.tmp # SQL import
 SQLTMP=$P/restype.tmp.sql # SQL temporary file
 
 # export oai data
-echo Exporting oai data to be classified...
-./resource_type_extract.sh 1 > $TMP1 
-#./resource_type_extract.sh all > $TMP1 
+#echo Exporting oai data to be classified...
+#time ./resource_type_extract.sh 36 > $TMPA 
+#time ./resource_type_extract.sh all > $TMPA 
+
+# clean up non-UTF-8 characters
+echo Scrubbing non-UTF-8 data
+time ./unicode_scrub.py $TMPA $TMP0
+
+# remove carriage-return (\r or CR or 0x0D)
+echo Cleaning out carriage returns...
+time tr -d '\r' < $TMP0 > $TMP1
 
 # delete existing enrichments that will be updated
 echo Removing existing enrichments for items that will be updated...
@@ -19,7 +29,7 @@ for id in `cut -f1 $TMP1`; do
         continue
     fi
 # while we're debugging, don't do this step, this allows us to see all previous classification attempts
-#    echo "delete from METADATA_ELEM WHERE TagName = 'type' and Item_ID = $id;" >> $SQLTMP ;
+    echo "delete from METADATA_ELEM WHERE TagName = 'type' and Item_ID = $id;" >> $SQLTMP ;
     echo "update ARCHIVED_ITEM SET TypeClassifiedDate = NOW() WHERE Item_ID = $id;" >> $SQLTMP ;
 done
 mysql --defaults-file=~/oai.my.cnf < $SQLTMP
@@ -27,19 +37,24 @@ mysql --defaults-file=~/oai.my.cnf < $SQLTMP
 echo Running binary classifier...
 cd classifier
 # run binary type classifier on oai data; output to $TMP2
-./restype_test.sh resourceTypeBinaryClassifier.mallet $TMP1 $TMP2
+#time ./restype_test.sh resourceTypeBinaryClassifier.mallet $TMP1 $TMP2
+time ./classify.sh binary $TMP1 $TMP2
 cd ..
 
+echo Importing binary classifier probabilities
+time ./import-binary-classified.py $TMP2
+
 echo Preparing for multi-type classifier...
-python prep_binary_for_multi.py $TMP1 $TMP2 $TMP3
+time python prep_binary_for_multi.py $TMP1 $TMP2 $TMP3
 
 echo Running multi-type classifier...
 cd classifier
 # run muli-type classifier on YES lang resources
 #CLASSIFIERCOMMENT='original resourceTypeClassifier.mallet'
-CLASSIFIERCOMMENT='multi_m-1 classifier 20100113'
+CLASSIFIERCOMMENT='classifier 20100714'
 #./restype_test.sh resourceTypeClassifier.mallet $TMP3 $TMP4
-./restype_test.sh resourceTypeMultiClassifier.mallet $TMP3 $TMP4
+#time ./restype_test.sh resourceTypeMultiClassifier.mallet $TMP3 $TMP4
+time ./classify.sh multi $TMP3 $TMP4
 cd ..
 
 echo Preparing enrichments for import...
@@ -56,7 +71,7 @@ mysql --defaults-file=~/oai.my.cnf < $SQLTMP
 
 echo Importing enrichments...
 # load into mysql
-mysql --defaults-file=~/oai.my.cnf -e "
+time mysql --defaults-file=~/oai.my.cnf -e "
 LOAD DATA LOCAL INFILE '$TMP5'
 INTO TABLE METADATA_ELEM
 FIELDS TERMINATED BY '\\t'
@@ -72,9 +87,9 @@ LINES TERMINATED BY '\\n'
 # Type = linguistic-type (or more accurately) resource-type
 
 # clean up tmp files
-rm $TMP1
-rm $TMP2
-rm $TMP3
+#rm $TMP1
+#rm $TMP2
+#rm $TMP3
 #rm $TMP4
-rm $TMP5
-rm $SQLTMP
+#rm $TMP5
+#rm $SQLTMP
