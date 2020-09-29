ea4d13d8ecdb68010720d34c1b7f4e5b959e1288
hleeldc <haejoong@ldc.upenn.edu>
2020-09-29 00:19:21 -0400

    more fixes for mysql configuration

--- system/start.sh
+++ system/start.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
-hostname > `cat /etc/olacbase`/conf/mysql/host
+confbase=`cat /etc/olacbase`/conf
+hostname > $confbase/mysql/host
+echo olac > $confbase/mysql/user
+echo olac123 > $confbase/mysql/passwd
 
 exec httpd -D FOREGROUND
