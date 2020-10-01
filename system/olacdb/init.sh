c6d5fbb1ae78b07de282a94065fe0fe1bd2239c6
hleeldc <haejoong@ldc.upenn.edu>
2020-09-30 20:55:44 -0400

    add monthly harvester database

--- system/olacdb/init.sh
+++ system/olacdb/init.sh
@@ -2,12 +2,20 @@
 
 OLACDB_SCHEMA="$1"
 
-if ! test -d /db/olac; then
-    mysql_install_db --user=mysql --datadir=/db
-    (
-        echo "create database olac;"
-        echo "grant all on olac.* to 'olac'@'%' identified by 'olac123';"
-        echo "use olac;"
-        cat "$OLACDB_SCHEMA"
-    ) | mysqld --user=mysql --datadir=/db --bootstrap --skip-grant-tables=off
-fi
+create_db()
+{
+    db=$1
+    if ! test -d /db/$db; then
+        mysql_install_db --user=mysql --datadir=/db
+        (
+            echo "create database $db;"
+            echo "grant all on $db.* to 'olac'@'%' identified by 'olac123';"
+            echo "use $db;"
+            cat "$OLACDB_SCHEMA"
+        ) | mysqld --user=mysql --datadir=/db --bootstrap --skip-grant-tables=off
+    fi
+}
+
+create_db olac
+create_db olac2
+
