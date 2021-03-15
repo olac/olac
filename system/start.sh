a4acd9f14248c3e346385b3a9675f4c7ae2a5be2
hleeldc <haejoong@ldc.upenn.edu>
2021-03-14 21:57:17 -0400

    remove init container

--- system/start.sh
+++ system/start.sh
@@ -2,8 +2,15 @@
 
 confbase=`cat /etc/olacbase`/conf
 hostname > $confbase/mysql/host
-echo olac > $confbase/mysql/user
-echo olac123 > $confbase/mysql/passwd
+echo $OLAC_MYSQL_USER > $confbase/mysql/user
+echo $OLAC_MYSQL_PASSWORD > $confbase/mysql/passwd
+
+for d in static static-records xmldump register/pending register/hosted; do
+    p=/olac/web/data/$d
+    mkdir -p $p
+    chgrp www-data $p
+    chmod g+ws $p
+done
 
 syslogd
 
